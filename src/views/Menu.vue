2fc06a362ac200144260606a7b3ff1ca197d3228
Da-Ronja <70214836+Da-Ronja@users.noreply.github.com>
2021-02-18 17:14:12 +0100

    Update dots on menu li

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -4,14 +4,18 @@
     <NavComp />
     <CartComp />
 
-    <div v-show="showMenu">
+    <div v-show="showMenu" class="menu-wrapper">
         <h2>Meny</h2>
         <ul>
           <li v-for="item in menu" :key="item.id">
-            <button class="add-btn" @click="addToCart(item)"></button>
-            <h3>{{ item.name }}</h3>
-            <h3>{{ item.price }}</h3>
-            <p>{{ item.description }}</p>
+            <div class="li-grid-menu">
+              <button class="add-btn" @click="addToCart(item)"></button>
+              <div class="name">
+                <h3>{{ item.name }}</h3>
+              </div>
+              <h3 class="price">{{ item.price }}</h3>
+              <p class="description">{{ item.description }}</p>
+            </div>
           </li>
         </ul>
     </div>
@@ -45,13 +49,50 @@ export default {
 </script>
 
 <style>
+.default-page {
+  background-color: var(--color3);
+}
+.menu-wrapper {
+  width: 80%;
+}
+.li-grid-menu {
+  min-width: 80%;
+  display: grid;
+  grid-template-columns: 15% 70% 15%;
+  grid-template-rows: 1fr auto;
+  justify-content: space-between;
+}
 .add-btn {
   width: 32px;
   height: 32px;
+  grid-column: 1;
+  grid-row-start: 1;
+  grid-row-end: 3;
   background: url('../assets/add-btn.svg');
   background-size: cover;
   border-radius: 50%;
   outline: none;
   border: none;
+}
+.name {
+  grid-column: 2;
+  grid-row: 1;
+  border-bottom: 2px dotted black;
+  text-align: left;
+}
+.name h3 {
+  outline: 4px solid var(--color3);
+  display: inline;
+}
+.price {
+  grid-column: 3;
+  grid-row: 1;
+}
+.description {
+  grid-column: 2;
+  grid-row: 2;
+  text-align: left;
+  margin-bottom: 20px;
+  font-size: 10px;
 }
 </style>
